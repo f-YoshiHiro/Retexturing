ed387e8d18851100e74da7cdb3a837a0b12c0f50
f-YoshiHiro <xxromancedawnxx20i23i2@gmail.com>
2016-01-25 19:04:40 +0900

    gary scale
    use gray scale image

--- source/main.cpp
+++ source/main.cpp
@@ -14,260 +14,210 @@
 using namespace cv;
 
 // -----------global---------- //
+Mat mat00H, mat19H;
+Mat Gmat00H;
+
 int HUGE_NUM = 10000;
 
-cv::Mat src, hsv, mask;
-
-EigenVector2i CornerTL, CornerTR, CornerBL, CornerBR;
-EigenMatrixXi V, T;
+int num_X = 5;
+int num_Y = 5;
 
 std::vector<Point> vertVec;
 
 
 // ----------functions---------- //
-void show_point(cv::Mat* mat, int i, int j);
+void load_images();
 
-void gen_mesh(EigenMatrixXi* Pmat, EigenMatrixXi* Imat, int NUM_Y, int NUM_X);
+void search_corners(Mat* mat, Point* TL, Point* TR, Point* BL, Point* BR);
 
-void draw_tri(Point p1, Point p2, Point p3, Scalar color=(Vec3b(255,0,255)), int thickness=2);
-void draw_mesh(EigenMatrixXi* V, EigenMatrixXi* T, int NUM_Y, int NUM_X);
+void gen_mesh(EigenMatrixXi* V, EigenMatrixXi* T, Point TL, Point TR, Point BL, Point BR, int NUM_Y, int NUM_X);
+
+void draw_vert(Mat* mat, EigenMatrixXi* V, int radius=3, Scalar color = (Vec3b(255, 0, 0)));
+void draw_tri(Mat* mat, Point p1, Point p2, Point p3, Scalar color=(Vec3b(255,0,255)), int thickness=1);
+void draw_mesh(Mat* mat, EigenMatrixXi* V, EigenMatrixXi* T, int NUM_Y, int NUM_X);
+
+void compute_GlobalTransformation(const Mat* mat_old, const Mat* mat_new, EigenVector2i d);
+
+unsigned char compute_GDerivativeX(const Mat* mat, int y, int x, int size = 3);
+unsigned char compute_GDerivativeY(const Mat* mat, int y, int x, int size = 3);
+unsigned char compute_GDerivativeT(const Mat* mat_old, const Mat* mat_new, int y, int x, int size = 3);
+
+Vec3b compute_DerivativeX(const Mat* mat, int y, int x, int size = 3);
+Vec3b compute_DerivativeY(const Mat* mat, int y, int x, int size = 3);
+Vec3b compute_DerivativeT(const Mat* mat_old, const Mat* mat_new, int y, int x, int size = 3);
 
 // ----------main---------- //
 int main(int argc, char** argv) 
 {
-	//cv::Mat src, hsv, mask;
+	// variable
+	Point TL, TR, BL, BR;	// corners
+	EigenMatrixXi V;	// vertices
+	EigenMatrixXi T;	// triangles
 
-	src = cv::imread("../../image/Sim0000.png");
-	cv::cvtColor(src, hsv, CV_BGR2HSV);
-
-	//int offset = 50;
-	int offset_low = 160;
-	int offset_high = -140;
-
-	//inRange(hsv, Scalar(31-offset, 165 - offset, 130 - offset), Scalar(51 + offset, 185 + offset, 150 + offset), mask);
-	//inRange(hsv, Scalar(160 - offset_low, 233 - offset_low, 214 - offset_low), Scalar(160+ offset_high, 233+ offset_high, 214+ offset_high), mask);
-	inRange(hsv, Scalar(82, 82, 82), Scalar(255, 255, 255), mask);
+	// load image
+	load_images();
 
 	// search corner
-	//EigenVector2i CornerTL, CornerTR, CornerBL, CornerBR;
+	search_corners(&Gmat00H, &TL, &TR, &BL, &BR);
+
+	// generate mesh
+	gen_mesh(&V, &T, TL, TR, BL, BR, num_Y, num_X);
+
+	// draw
+	draw_mesh(&Gmat00H, &V, &T, num_Y, num_X);
+	draw_vert(&Gmat00H, &V);
+
+	cv::namedWindow("src", cv::WINDOW_AUTOSIZE);
+	imshow("src", mat00H);
+
+	cv::namedWindow("gray", cv::WINDOW_AUTOSIZE);
+	imshow("gray", Gmat00H);
+
+	{
+		//// test del
+		//Mat matDelY(Size(Gmat00H.cols, Gmat00H.rows), CV_8UC1);
+
+		//for (int y = 1; y < Gmat00H.rows-1; y++)
+		//{
+		//	for (int x = 1; x < Gmat00H.cols-1; x++)
+		//	{
+		//		//std::cout << y << "," << x << std::endl;
+		//		matDelY.at<unsigned char>(y, x) = compute_GDerivativeY(&Gmat00H, y, x);
+		//	}
+		//}
+		//cv::namedWindow("dely", cv::WINDOW_AUTOSIZE);
+		//imshow("dely", matDelY);
+
+		//Mat matDelX(Size(Gmat00H.cols, Gmat00H.rows), CV_8UC1);
+
+		//for (int y = 1; y < Gmat00H.rows - 1; y++)
+		//{
+		//	for (int x = 1; x < Gmat00H.cols - 1; x++)
+		//	{
+		//		//std::cout << y << "," << x << std::endl;
+		//		matDelX.at<unsigned char>(y, x) = compute_GDerivativeX(&Gmat00H, y, x);
+		//	}
+		//}
+		//cv::namedWindow("delx", cv::WINDOW_AUTOSIZE);
+		//imshow("delx", matDelX);
+	}
+
+	waitKey(0);
+
+	return 0;
+}
+
+#pragma region functions
+// -------------------------------------------------- //
+// Load Images
+// -------------------------------------------------- //
+void load_images()
+{
+	mat00H = cv::imread("../../image/Sim0000H.png");
+	mat19H = cv::imread("../../image/Sim0019H.png");
+
+	Gmat00H = cv::imread("../../image/Sim0000H.png", IMREAD_GRAYSCALE);
+}
+
+// -------------------------------------------------- //
+// Search Corners
+//
+// mat:image
+// TL: top left
+// TR: top right
+// BL: bottom left
+// BR: bottom right
+// -------------------------------------------------- //
+void search_corners(Mat* mat, Point* TL, Point* TR, Point* BL, Point* BR)
+{
+	// init
+	TL->y = HUGE_NUM;	TL->x = HUGE_NUM;
+	TR->y = HUGE_NUM;	TR->x = HUGE_NUM;
+	BR->y = 0;			BR->x = 0;
+	BL->y = 0;			BL->x = 0;
+
 	ScalarType LNormMIN = 10000.0;	ScalarType RNormMIN = 10000.0;
 	ScalarType LNormMAX = 0.0;		ScalarType RNormMAX = 0.0;
 
-	CornerTL[0] = HUGE_NUM; CornerTL[1] = HUGE_NUM;
-	CornerTR[0] = HUGE_NUM; CornerTR[1] = HUGE_NUM;
-	CornerBR[0] = 0;        CornerBR[1] = 0;
-	CornerBL[0] = 0;        CornerBL[1] = 0;
-
-	for (int i = 0; i < src.rows; i++) // rows
+	for (int y = 0; y < mat->rows; y++) // rows
 	{
-		for (int j = 0; j < src.cols; j++) // cols
+		for (int x = 0; x < mat->cols; x++) // cols
 		{
-			Vec3b bgr = src.at<Vec3b>(i,j);
+			unsigned char pixel = mat->at<unsigned char>(y, x);
 
 			// skip judge
 			bool skip_flag = true;
 
-			if (bgr != cv::Vec3b(0, 0, 0)) { skip_flag = false; }
+			if (pixel != (unsigned char)0) { skip_flag = false; }
 
 			// search corner
 			if (skip_flag == false)
 			{
 				// comp norm
-				int cols = src.cols;
-				ScalarType LNorm = sqrt( (i*i) + (j*j));
-				ScalarType RNorm = sqrt((i*i) + ((j-cols)*(j-cols)));
+				int cols = mat->cols;
+				ScalarType LNorm = sqrt((y*y) + (x*x));
+				ScalarType RNorm = sqrt((y*y) + ((x - cols)*(x - cols)));
 
 
 				if (LNorm < LNormMIN)
 				{
 					LNormMIN = LNorm;
-					CornerTL[0] = i;	CornerTL[1] = j;
+					TL->y = y;	TL->x = x;
 				}
 				else if (LNorm > LNormMAX)
 				{
 					LNormMAX = LNorm;
-					CornerBR[0] = i;	CornerBR[1] = j;
+					BR->y = y;	BR->x = x;
 				}
 
 				if (RNorm < RNormMIN)
 				{
 					RNormMIN = RNorm;
-					CornerTR[0] = i;	CornerTR[1] = j;
+					TR->y = y;	TR->x = x;
 				}
 				else if (RNorm > RNormMAX)
 				{
 					RNormMAX = RNorm;
-					CornerBL[0] = i;	CornerBL[1] = j;
+					BL->y = y;	BL->x = x;
 				}
 			}
 		}
 	}
+	std::cout << "Corners" << std::endl;
+	std::cout << "TL:" << TL->y << "," << TL->x <<std::endl;
+	std::cout << "TR:" << TR->y << "," << TR->x <<std::endl;
+	std::cout << "BL:" << BL->y << "," << BL->x <<std::endl;
+	std::cout << "BR:" << BR->y << "," << BR->x <<std::endl;
 
-	//CornerTR[0] = CornerTL[0];	CornerTR[1] = CornerBR[1];
-	//CornerBL[0] = CornerBR[0];  CornerBL[1] = CornerTL[1];
-
-
-	std::cout << "CornerTL" << std::endl;	std::cout << CornerTL << std::endl;
-	std::cout << "CornerTR" << std::endl;	std::cout << CornerTR << std::endl;
-	std::cout << "CornerBL" << std::endl;	std::cout << CornerBL << std::endl;
-	std::cout << "CornerBR" << std::endl;	std::cout << CornerBR << std::endl;
-
-	// generate mesh
-	gen_mesh(&V, &T, 5, 5);
-
-	//show_point(&src, CornerTL[0], CornerTL[1]);
-	//show_point(&src, CornerTR[0], CornerTR[1]);
-	//show_point(&src, CornerBL[0], CornerBL[1]);
-	//show_point(&src, CornerBR[0], CornerBR[1]);
-
-	Point p1, p2;
-	p1.x = CornerTL[1]; p1.y = CornerTL[0];
-	p2.x = CornerBR[1]; p2.y = CornerBR[0];
-
-	// line test
-	//line(src, p1, p2, Scalar(Vec3b(255, 0, 255)), 2, 8, 0);
-
-	cv::namedWindow("src", cv::WINDOW_AUTOSIZE);
-	imshow("src", src);
-	cv::namedWindow("mask", cv::WINDOW_AUTOSIZE);
-	imshow("mask", mask);
-
-	waitKey(0);
-
-
-	//-----------------------------------------------------------------------------
-
-	//Rect roi(0,0, 1857,1057);
-	//cv::Mat src, dst, mask, bg, fg;
-
-	//// input image as grey scale
-	//src = cv::imread("../../image/shape0.png");
-	//// if failed return -1
-	//if (src.empty())
-	//{
-	//	std::cerr << "Failed to open image file." << std::endl;
-	//	return -1;
-	//}
-
-	//cv::grabCut(src, dst, roi, bg, fg, 1, GC_INIT_WITH_RECT);
-	//cv::compare(dst, GC_PR_FGD, mask, CMP_EQ);
-
-	//src.copyTo(dst, mask);
-
-	//cv::namedWindow("src", cv::WINDOW_AUTOSIZE);
-	//imshow("src", src);
-	//cv::namedWindow("dst", cv::WINDOW_AUTOSIZE);
-	//imshow("dst", dst);
-	//waitKey();
-
-
-	//-----------------------------------------------------------------------------
-
-	//// input image as grey scale
-	//cv::Mat src = cv::imread("../../image/shape0.png", cv::IMREAD_GRAYSCALE);
-
-	//// if failed return -1
-	//if (src.empty())
-	//{
-	//	std::cerr << "Failed to open image file." << std::endl;
-	//	return -1;
-	//}
-
-	//// binalize
-	//cv::Mat bin;
-	//cv::threshold(src, bin, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-
-	//// make image for label
-	//cv::Mat labelImage(src.size(), CV_32S);
-
-	//// labeling
-	//int nLabels = cv::connectedComponents(bin, labelImage, 8);
-
-	//// decide color for rendering
-	//std::vector<cv::Vec3b> colors(nLabels);
-	//colors[0] = cv::Vec3b(0, 0, 0);
-	//for (int label = 0; label < nLabels; ++label)
-	//{
-	//	colors[label] = cv::Vec3b((rand()&255),(rand()&255),(rand()&255));
-	//}
-
-	//// render result
-	//cv::Mat dst(src.size(), CV_8UC3);
-	//for (int y = 0; y < dst.rows; ++y)
-	//{
-	//	for (int x = 0; x < dst.cols; ++x)
-	//	{
-	//		int label = labelImage.at<int>(y, x);
-	//		cv::Vec3b &pixel = dst.at<cv::Vec3b>(y, x);
-	//		pixel = colors[label];
-	//	}
-	//}
-	//cv::namedWindow("Source", cv::WINDOW_AUTOSIZE);
-	//cv::imshow("Source", src);
-
-	//cv::namedWindow("Connected Components", cv::WINDOW_AUTOSIZE);
-	//cv::imshow("Connected Components", dst);
-
-	//cv::waitKey(0 );
-
-	//-----------------------------------------------------------------------------
-
-
-	//Mat im = imread("../../image/Desert.jpg");
-
-	//if (im.data == NULL) 
-	//{	
-	//	std::cout << " imput  fail" << std::endl;
-	//	return -1; 
-	//}
-
-	//// make window
-	//namedWindow("desert", CV_WINDOW_AUTOSIZE);
-	//imshow("desert", im);
-
-	//
-	//waitKey(0);
-	//destroyWindow("desert");
-
-	//imwrite("../../image/output.bmp", im);
-
-	return 0;
+	int rudius = 3;
+	//circle(*mat, *TL, rudius, (Vec3b(255, 0, 0)), -1, 8, 0);
+	//circle(*mat, *TR, rudius, (Vec3b(255, 0, 0)), -1, 8, 0);
+	//circle(*mat, *BL, rudius, (Vec3b(255, 0, 0)), -1, 8, 0);
+	//circle(*mat, *BR, rudius, (Vec3b(255, 0, 0)), -1, 8, 0);
 }
 
-void show_point(cv::Mat* mat,int i, int j)
-{
-	Vec3b color_bgr = (0,0,255);
-	int size = 3;
-
-	for (int height = i-size; height < i+size; height++)
-	{
-		Vec3b* ptr = mat->ptr<Vec3b>(height);
-
-		for (int width = j-size; width < j+size; width++)
-		{
-			ptr[width] = color_bgr;
-		}
-	}
-}
-
+// -------------------------------------------------- //
+// Generate Mesh
+//
 // Pmat:pos mat
 // Imat:Tri index mat
 // i:row #vert
 // j:col #vert
-void gen_mesh(EigenMatrixXi* Pmat, EigenMatrixXi* Imat, int NUM_Y, int NUM_X)
+// -------------------------------------------------- //
+void gen_mesh(EigenMatrixXi* V, EigenMatrixXi* T, Point TL, Point TR, Point BL, Point BR, int NUM_Y, int NUM_X)
 {
-	Pmat->resize(NUM_Y*NUM_X,2);
-	Pmat->setZero();
+	V->resize(NUM_Y*NUM_X,2);
+	V->setZero();
 
-	Imat->resize(2*(NUM_Y-1)*(NUM_X-1), 3);
-	Imat->setZero();
+	T->resize(2*(NUM_Y-1)*(NUM_X-1), 3);
+	T->setZero();
 
 	//std::cout << "V.rows:" << Pmat->rows() << std::endl;
 	//std::cout << "T.rows:" << Imat->rows() << std::endl;
 
 	int dely, delx;
-	dely = (double)(CornerBL[0] - CornerTL[0])/(double)(NUM_Y-1.0);
-	delx = (double)(CornerTR[1] - CornerTL[1])/(double)(NUM_X-1.0);
+	dely = (double)(BL.y - TL.y)/(double)(NUM_Y-1.0);
+	delx = (double)(TR.x - TL.x)/(double)(NUM_X-1.0);
 
 	std::cout << "dely:" << dely << std::endl;
 	std::cout << "delx:" << delx << std::endl;
@@ -279,8 +229,8 @@ void gen_mesh(EigenMatrixXi* Pmat, EigenMatrixXi* Imat, int NUM_Y, int NUM_X)
 		{
 			//std::cout << "(" << y << "," << x << ")" << std::endl;
 
-			(*Pmat)((NUM_X*y)+x, 0) = CornerTL[0] + (y*dely); //y
-			(*Pmat)((NUM_X*y)+x, 1) = CornerTL[1] + (x*delx); //x
+			(*V)((NUM_X*y)+x, 0) = TL.y + (y*dely); //y
+			(*V)((NUM_X*y)+x, 1) = TL.x + (x*delx); //x
 
 			//show_point(&src, CornerTL[0] + (y*dely), CornerTL[1] + (x*delx));
 		}
@@ -294,102 +244,167 @@ void gen_mesh(EigenMatrixXi* Pmat, EigenMatrixXi* Imat, int NUM_Y, int NUM_X)
 			int triIndex;
 
 			triIndex = ( (((NUM_X - 1)*y) + x) * 2 )+0;
-			(*Imat)(triIndex, 0) = (NUM_X*y)     + x;
-			(*Imat)(triIndex, 1) = (NUM_X*y)     + (x+1);
-			(*Imat)(triIndex, 2) = (NUM_X*(y+1)) + (x+1);
+			(*T)(triIndex, 0) = (NUM_X*y)     + x;
+			(*T)(triIndex, 1) = (NUM_X*y)     + (x+1);
+			(*T)(triIndex, 2) = (NUM_X*(y+1)) + (x+1);
 
 			triIndex = ((((NUM_X - 1)*y) + x) * 2) + 1;
-			(*Imat)(triIndex, 0) = (NUM_X*y)     + x;
-			(*Imat)(triIndex, 2) = (NUM_X*(y+1)) + (x + 1);
-			(*Imat)(triIndex, 1) = (NUM_X*(y+1)) + x;
+			(*T)(triIndex, 0) = (NUM_X*y)     + x;
+			(*T)(triIndex, 2) = (NUM_X*(y+1)) + (x + 1);
+			(*T)(triIndex, 1) = (NUM_X*(y+1)) + x;
 		}
 	}
+}
 
-	// show vert
-	for (int y = 0; y < NUM_Y; y++)
+// -------------------------------------------------- //
+// Draw Vertices
+// -------------------------------------------------- //
+void draw_vert(Mat* mat,EigenMatrixXi* V, int radius, Scalar color)
+{
+	for (int i = 0; i < V->rows(); i++)
 	{
-		for (int x = 0; x < NUM_X; x++)
-		{
-			show_point(&src, (*Pmat)((NUM_X*y) + x, 0), (*Pmat)((NUM_X*y) + x, 1));
-		}
-	}
+		Point p;
+		p.x = (*V)(i, 1);	p.y = (*V)(i, 0);
 
-	// show Tri
-	for (int i = 0; i < Imat->rows(); i++)
+		circle(*mat, p, radius, color, -1, 8, 0);
+	}
+}
+
+// -------------------------------------------------- //
+// Draw Triangle 
+// -------------------------------------------------- //
+void draw_tri(Mat* mat, Point p1, Point p2, Point p3, Scalar color, int thickness)
+{
+	line(*mat, p1, p2, color, thickness, 8, 0);
+	line(*mat, p2, p3, color, thickness, 8, 0);
+	line(*mat, p3, p1, color, thickness, 8, 0);
+}
+
+// -------------------------------------------------- //
+// Draw Mesh
+// -------------------------------------------------- //
+void draw_mesh(Mat* mat, EigenMatrixXi* V, EigenMatrixXi* T, int NUM_Y, int NUM_X)
+{
+	for (int i = 0; i < T->rows(); i++)
 	{
 		Point p1, p2, p3;
 		int   i1, i2, i3;
 
-		i1 = (*Imat)(i, 0);
-		i2 = (*Imat)(i, 1);
-		i3 = (*Imat)(i, 2);
+		i1 = (*T)(i, 0);	i2 = (*T)(i, 1);	i3 = (*T)(i, 2);
 
-		p1.x = (*Pmat)(i1, 1);	p1.y = (*Pmat)(i1, 0);
-		p2.x = (*Pmat)(i2, 1);	p2.y = (*Pmat)(i2, 0);
-		p3.x = (*Pmat)(i3, 1);	p3.y = (*Pmat)(i3, 0);
+		p1.x = (*V)(i1, 1);	p1.y = (*V)(i1, 0);
+		p2.x = (*V)(i2, 1);	p2.y = (*V)(i2, 0);
+		p3.x = (*V)(i3, 1);	p3.y = (*V)(i3, 0);
 
-		draw_tri(p1, p2, p3);
+		draw_tri(mat, p1, p2, p3);
+	}
+}
+
+// -------------------------------------------------- //
+// Compute Global Transformation
+// -------------------------------------------------- //
+void compute_GlobalTransformation(const Mat* mat_old, const Mat* mat_new, EigenVector2i d)
+{
+	if(((mat_old->rows)!=(mat_new->rows))|| ((mat_old->cols) != (mat_new->cols)))
+	{
+		std::cerr << "mat size is different compute_GlobalTransformation" << std::endl;
 	}
 
+	std::vector<SparseMatrixTriplet> a_triplets;	a_triplets.clear();
 
-
-	// set
-
-
-	/*for (int height = 0; height < i; height++)
+	for (int y = 1; y < mat_old->rows-1; y++)
 	{
-		for (int width = 0; width < j; width++)
+		for (int x = 1; x < mat_old->cols-1; x++)
 		{
-			if(height==0 && width==0)
-			{ 
-				(*Pmat)(0, 0) = CornerTL[0];
-				(*Pmat)(0, 1) = CornerTL[1];
-			}
-			else
+			Vec3b delY = compute_DerivativeY(mat_new, y, x);
+			Vec3b delX = compute_DerivativeX(mat_new, y, x);
+			Vec3b zero;
+
+			/*if ((delY!=(Vec3b)(0,0,0))&&(delX!=(Vec3b)(0,0,0)))
 			{
-				double iwight = (double)width / (double)j;
-				double jwight = (double)height / (double)i;
 
-				EigenVector2i ivec, jvec;
-
-				ivec[0] = ((1.0 - iwight)*(double)Lvec[0]) + (iwight*(double)Lvec[0]);
-				ivec[1] = ((1.0 - iwight)*(double)Lvec[1]) + (iwight*(double)Lvec[1]);
-
-				jvec[0] = ((1.0 - jwight)*(double)Tvec[0]) + (jwight*(double)Bvec[0]);
-				jvec[1] = ((1.0 - jwight)*(double)Tvec[1]) + (jwight*(double)Bvec[1]);
-
-				(*Pmat)((height*j) + width, 0) = ;
-				(*Pmat)((height*j) + width, 1) = ;
-			}
-
-		}
-	}*/
-}
-
-
-// Draw Triangle 
-void draw_tri(Point p1, Point p2, Point p3, Scalar color, int thickness)
-{
-	line(src, p1, p2, color, thickness, 8, 0);
-	line(src, p2, p3, color, thickness, 8, 0);
-	line(src, p3, p1, color, thickness, 8, 0);
-}
-
-// Draw Mesh
-void draw_mesh(EigenMatrixXi* V, EigenMatrixXi* T, int NUM_Y, int NUM_X)
-{
-	for (int y = 0; y < NUM_Y - 1; y++)
-	{
-		for (int x = 0; x < NUM_X - 1; x++)
-		{
-			int triIndex;
-			Point p1, p2, p3, p4;
-
-			triIndex = ((((NUM_X - 1)*y) + x) * 2) + 0;
-			
-
-			triIndex = ((((NUM_X - 1)*y) + x) * 2) + 1;
-			
+			}*/
 		}
 	}
+
+
 }
+
+// -------------------------------------------------- //
+// Compute Grayscale Derivative X
+// -------------------------------------------------- //
+unsigned char compute_GDerivativeX(const Mat* mat, int y, int x, int size)
+{
+	unsigned char pixel_p = mat->at<unsigned char>(y, x + 1);
+	unsigned char pixel_m = mat->at<unsigned char>(y, x - 1);
+
+	unsigned char value = pixel_p - pixel_m;
+
+	return value;
+}
+
+// -------------------------------------------------- //
+// Compute Grayscale Derivative Y
+// -------------------------------------------------- //
+unsigned char compute_GDerivativeY(const Mat* mat, int y, int x, int size)
+{
+	unsigned char pixel_p = mat->at<unsigned char>(y + 1, x);
+	unsigned char pixel_m = mat->at<unsigned char>(y - 1, x);
+
+	unsigned char value = pixel_p - pixel_m;
+
+	return value;
+}
+
+// -------------------------------------------------- //
+// Compute Grayscale Derivative T
+// -------------------------------------------------- //
+unsigned char compute_GDerivativeT(const Mat* mat_old, const Mat* mat_new, int y, int x, int size)
+{
+	unsigned char pixel_old = mat_old->at<unsigned char>(y, x);
+	unsigned char pixel_new = mat_new->at<unsigned char>(y, x);
+
+	unsigned char value = pixel_new - pixel_old;
+
+	return value;
+}
+
+// -------------------------------------------------- //
+// Compute Derivative X
+// -------------------------------------------------- //
+Vec3b compute_DerivativeX(const Mat* mat, int y, int x, int size)
+{
+	Vec3b pixel_p = mat->at<Vec3b>(y, x+1);
+	Vec3b pixel_m = mat->at<Vec3b>(y, x-1);
+
+	Vec3b value = pixel_p - pixel_m;
+
+	return value;
+}
+
+// -------------------------------------------------- //
+// Compute Derivative Y
+// -------------------------------------------------- //
+Vec3b compute_DerivativeY(const Mat* mat, int y, int x, int size)
+{
+	Vec3b pixel_p = mat->at<Vec3b>(y+1, x);
+	Vec3b pixel_m = mat->at<Vec3b>(y-1, x);
+
+	Vec3b value = pixel_p - pixel_m;
+
+	return value;
+}
+
+// -------------------------------------------------- //
+// Compute Derivative T
+// -------------------------------------------------- //
+Vec3b compute_DerivativeT(const Mat* mat_old, const Mat* mat_new, int y, int x, int size)
+{
+	Vec3b pixel_old = mat_old->at<Vec3b>(y, x);
+	Vec3b pixel_new = mat_new->at<Vec3b>(y, x);
+
+	Vec3b value = pixel_new - pixel_old;
+
+	return value;
+}
+#pragma endregion
